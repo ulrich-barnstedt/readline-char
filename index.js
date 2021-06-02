a574ab572bb1f740b66cab814d64480cd7c5e689
Ulrich Barnstedt <ulrich.barnstedt+git@gmail.com>
2021-06-02 10:23:38 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+
+module.exports = class {
+    constructor () {
+        this.listeners = [];
+        this.cleanupHandlers = [];
+        this.blocked = false;
+
+        process.stdin.on("keypress", (str, key) => {
+            if (key.sequence === '\u0003') {
+                this.cleanupHandlers.forEach(h => h());
+                process.exit();
+            }
+
+            if (this.blocked) return;
+            this.listeners.forEach(l => l(key));
+        });
+    }
+
+    bind (listener) {
+        this.listeners.push(listener);
+    }
+
+    bindCleanup (handler) {
+        this.cleanupHandlers.push(handler);
+    }
+
+    init () {
+        readline.emitKeypressEvents(process.stdin);
+        if (process.stdin.isTTY) process.stdin.setRawMode(true);
+    }
+
+    block () {
+        this.blocked = true;
+    }
+
+    unblock () {
+        this.blocked = false;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+## readline-char
+`$ npm install readline-char`
+
+Usage
+```js
+const Rlc = require("readline-char");
+
+//create an instance
+let input = new Rlc();
+
+//add an listener
+input.bind((key) => {console.log(key)});
+
+//add an cleanup listener (on ctrl+c)
+input.bindCleanup(() => {
+    //cleanup code
+})
+
+//start listening for events
+input.init();
+
+//block all listeners
+input.block();
+
+//unblock all listeners
+input.unblock();
+```
